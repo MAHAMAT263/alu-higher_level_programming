c3c2b2490fdf210d827b95a08898e0193125af42
MAHAMAT263 <m.ahmat@alustudent.com>
2023-07-10 14:50:23 -0700

    #

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 for c in range(ord('a'), ord('z')+1):
     print('{}'.format(chr(c)), end='')
-    
-    
-#import string
-#print(string.ascii_lowercase)
-
-#import string
-#v = string.ascii_lowercase
-#print('{}'.format(v))"""
