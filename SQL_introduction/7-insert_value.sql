95dfa74e916e458e12ad0320834605a8694064fc
MAHAMAT263 <m.ahmat@alustudent.com>
2023-08-04 07:38:16 -0700

    #

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,4 @@
--- insert a new row of data in this table in this database
+-- A script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server
+-- Query to insert a new row
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
 
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Holberton School');
