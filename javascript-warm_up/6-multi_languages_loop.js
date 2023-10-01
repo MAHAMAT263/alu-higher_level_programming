c09a2b574f9f6fa49d953ef2da22a692b3e51e0a
MAHAMAT263 <m.ahmat@alustudent.com>
2023-10-01 08:12:14 -0700

    #

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+function loopToLanguages () {
+  let n = 0;
+  while (n < 1) {
+    console.log(`C is fun
+Python is cool
+JavaScript is amazing`);
+    n++;
+  }
+}
+loopToLanguages();
